5b95fa51fa0e7ac58da01fdfaf1ca18456b03afd
xzhen <zhenxin77528@outlook.com>
2019-02-26 14:01:50 +0800

    update

---
+++ src/main/java/com/zhenxin/chamberlain/common/Test.java
@@ -0,0 +1,9 @@
+package com.zhenxin.chamberlain.common;
+
+/**
+ * @author xzhen
+ * @created 13:57 26/02/2019
+ * @description TODO
+ */
+public class Test {
+}
---
+++ src/main/java/com/zhenxin/chamberlain/config/BeanConfig.java
@@ -0,0 +1,14 @@
+package com.zhenxin.chamberlain.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author xzhen
+ * @created 13:43 26/02/2019
+ * @description TODO
+ */
+@Configuration
+public class BeanConfig {
+
+
+}
