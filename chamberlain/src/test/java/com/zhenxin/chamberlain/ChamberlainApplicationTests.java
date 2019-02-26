8a5f7fb1b47057e744c24cc6c92b5e0d95785019
xzhen <zhenxin77528@outlook.com>
2019-02-26 15:19:49 +0800

    update

---
+++ chamberlain/src/test/java/com/zhenxin/chamberlain/ChamberlainApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhenxin.chamberlain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChamberlainApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
