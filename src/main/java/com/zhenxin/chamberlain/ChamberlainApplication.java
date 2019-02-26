1458c66f3c076d632b706ca1d2ab14d2835dbe58
xzhen <zhenxin77528@outlook.com>
2019-02-26 13:40:41 +0800

    init

---
+++ src/main/java/com/zhenxin/chamberlain/ChamberlainApplication.java
@@ -0,0 +1,13 @@
+package com.zhenxin.chamberlain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamberlainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChamberlainApplication.class, args);
+    }
+
+}
